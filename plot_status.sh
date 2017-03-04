d5a1b0eff7991bd2fa4cc4ab1092518cb98b153c
Marcus <best.nose@gmail.com>
2017-03-04 15:52:19 +1300

    sorted mqtt, rrd and pyserial

---
+++ plot_status.sh
@@ -0,0 +1,7 @@
+rrdtool graph status_plot.png -a PNG \
+'DEF:f=/tmp/boiler/flame.rrd:boiler_flame:AVERAGE' \
+'DEF:t_water=/tmp/boiler/temp/water.rrd:boiler_temp_water:AVERAGE' \
+'DEF:t_auger=/tmp/boiler/temp/auger.rrd:boiler_temp_auger:AVERAGE' \
+'LINE1:f#ff0000:Flame' \
+'LINE2:t_water#00ff00:Water Temperature' \
+'LINE3:t_auger#0000ff:Auger Temperature' \
--- python/serial_listen.py
+++ python/serial_listen.py
@@ -1,17 +1,61 @@
 import serial
 import time
+#import paho.mqtt.client as mqtt
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("boiler/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+' '+str(msg.payload))
+    allowed_passthrough_msg = ['Turn Off Boiler', 'Turn On Boiler']
+    if str(msg.payload) in allowed_passthrough_msg:
+	port.write('\r\n'+str(msg.payload)+'\r')
+        print 'Sent ' + msg.payload + ' to serial port.'
+
+
 
 def readlineCR(port):
-        rv = ""
-        while True:
+    rv = ""
+    while True:
         ch = port.read()
         rv += ch
-        if ch=='\r' or ch=='':
-        return rv
+        if ch=='\r':# or ch=='':
+            if 'MQTT' in rv:
+                print rv
+                received = rv[6:]
+                received_splited = received.split('/')
+                topic = '/'.join(received_splited[:-1])
+                payload = received_splited[-1]
+                print topic, payload
+                publish.single(topic, payload, auth=auth, hostname="houseslave")
+            return rv
 
+
+auth = {'username':"esp", 'password':"heating"}
 port = serial.Serial("/dev/ttyUSB0", baudrate=115200, timeout=3.0)
 
-while True:
-        port.write("\r\nSay something:")
-        rcv = readlineCR(port)
-        port.write("\r\nYou sent:" + repr(rcv))
+if __name__ == "__main__":
+    client = mqtt.Client()
+    client.username_pw_set(username='esp', password='heating')
+    
+    #mqtt.userdata_set(username='esp',password='heating')
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect("houseslave", 1883, 60)
+
+    # Blocking call that processes network traffic, dispatches callbacks and
+    # handles reconnecting.
+    # Other loop*() functions are available that give a threaded interface and a
+    # manual interface.
+    # client.loop_forever()
+    client.loop_start()
+    while True:
+        readlineCR(port)
+        #print rcv
