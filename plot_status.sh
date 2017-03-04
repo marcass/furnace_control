cb7d1c5e95a775c1196ecdc07162b1a3a59be39f
marcus <best.nose@gmail.com>
2017-03-04 17:34:15 +1300

    updated plot file

--- plot_status.sh
+++ plot_status.sh
@@ -1,7 +1,9 @@
-rrdtool graph status_plot.png -a PNG \
+rrdtool graph /home/marcus/status_plot.png -a PNG \
 'DEF:f=/tmp/boiler/flame.rrd:boiler_flame:AVERAGE' \
 'DEF:t_water=/tmp/boiler/temp/water.rrd:boiler_temp_water:AVERAGE' \
 'DEF:t_auger=/tmp/boiler/temp/auger.rrd:boiler_temp_auger:AVERAGE' \
 'LINE1:f#ff0000:Flame' \
 'LINE2:t_water#00ff00:Water Temperature' \
 'LINE3:t_auger#0000ff:Auger Temperature' \
+
+scp /home/marcus/status_plot.png mw@htpc:~/
