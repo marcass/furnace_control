ef683ee39c2017955a1f8449b37e8edec5b9e90b
marcus <best.nose@gmail.com>
2017-03-08 13:07:47 +1300

    more python stuff

--- python/arduino_comms.py
+++ python/arduino_comms.py
@@ -1,10 +1,48 @@
 import serial
 import time
-#import paho.mqtt.client as mqtt
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
+import threading
+import logging
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='(%(threadName)-9s) %(message)s',)
 ser = serial
+
+def wait_for_arduino(e):
+    logging.debug('wait_for_arduino starting')
+    arduino_connected = e.wait()
+    logging.debug('arduino connection occured: %s', arduino_connected)
+
+def wait_for_arduino_timeout(e, t):
+    while not e.isSet():
+        logging.debug('wait_for_arduino connection_timeout starting')
+        arduino_connected = e.wait(t)
+        logging.debug('arduino connection occured: %s', arduino_connected)
+        # need a test in here?
+        # http://www.bogotobogo.com/python/Multithread/python_multithreading_Event_Objects_between_Threads.php
+        if arduino_connected:
+            client = mqtt.Client()
+            client.username_pw_set(username='esp', password='heating')
+
+            #mqtt.userdata_set(username='esp',password='heating')
+            client.on_connect = on_connect
+            client.on_message = on_message
+            client.connect("houseslave", 1883, 60)
+
+            # Blocking call that processes network traffic, dispatches callbacks and
+            # handles reconnecting.
+            # Other loop*() functions are available that give a threaded interface and a
+            # manual interface.
+            # client.loop_forever()
+            client.loop_start()
+            while True:
+                #for debugging enable printing of serial port data
+                rcv = readlineCR(ser)
+                #print rcv
+        else:
+            logging.debug('arduino not connected')
+
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -39,12 +77,16 @@ def readlineCR(port):
                 publish.single(topic, payload, auth=auth, hostname="houseslave")
             return rv
 
+ser = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=5.0)
+#ser = serial.Serial("/dev/arduino", baudrate=9600, timeout=5.0) 
+
 
 auth = {'username':"esp", 'password':"heating"}
 
 if __name__ == "__main__":
     #keep checking for port with a timeout of 5 seconds
     # need to modify to a threading thingy? https://docs.python.org/3/library/threading.html#event-objects
+    # while and try loops: http://stackoverflow.com/questions/2083987/how-to-retry-after-exception-in-python
     #if an event object can run a thread that checks for serial port /dev/arduino, set() internal
     # flag to true such that subscription stuff can start, or clear() when port not available
     # so it can keep checking
@@ -52,30 +94,17 @@ if __name__ == "__main__":
         ser = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=5.0)        
         #ser = serial.Serial("/dev/arduino", baudrate=9600, timeout=5.0) 
         #we have a serial port if it can be read
-        while ser.read():
-            client = mqtt.Client()
-            client.username_pw_set(username='esp', password='heating')
         
-            #mqtt.userdata_set(username='esp',password='heating')
-            client.on_connect = on_connect
-            client.on_message = on_message
-            client.connect("houseslave", 1883, 60)
-        
-            # Blocking call that processes network traffic, dispatches callbacks and
-            # handles reconnecting.
-            # Other loop*() functions are available that give a threaded interface and a
-            # manual interface.
-            # client.loop_forever()
-            client.loop_start()
-            while True:
-                #for debugging enable printing of serial port data
-                rcv = readlineCR(ser)
-                #print rcv
-        
-        #no serial port available
-        print 'serial closed'
-        ser.close()
-
-    #exception handling
-    except serial.serialutil.SerialException:
-        print 'exception'
+    e = threading.Event()
+    t1 = threading.Thread(name='blocking', 
+                          target=wait_for_arduino,
+                          args=(e,))
+    t1.start()
+    t2 = threading.Thread(name='non-blocking', 
+                          target=wait_for_arduino_timeout, 
+                          args=(e, 10))
+    t2.start()
+    logging.debug('Waiting before calling Event.set()')
+    time.sleep(10)
+    e.set()
+    logging.debug('Arduino attached')
